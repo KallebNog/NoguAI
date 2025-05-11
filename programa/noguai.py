f6bb0b8f0baabf0f8bb33d9684911fa29e18d184
Kalleb <kallebnog@gmail.com>
2025-05-11 20:09:53 -0300

    Começando

---
+++ programa/noguai.py
@@ -0,0 +1,8 @@
+import math
+
+perg = str(input('Qual será sua pergunta: ')).strip().lower()
+
+if perg == 'calculo':
+    perg = int(input('Oque deseja calcular: ')).strip().lower().split()
+
+    
